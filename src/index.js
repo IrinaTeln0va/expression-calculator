479dc5939f789885579ad64584017e6f503b3db7
IrinaTeln0va <ira8365@mail.ru>
2020-03-12 10:09:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+const innerBracketsReg = /\(([^\(\)]+)\)/;
 
 function expressionCalculator(expr) {
-    // write your solution here
+    while (expr.match(innerBracketsReg)) {
+        const innerBracketsExpr = expr.match(innerBracketsReg);
+        resultInsideBrackets = evaluateLinearExpr(innerBracketsExpr[1]);
+
+        expr = expr.replace(innerBracketsExpr[0], resultInsideBrackets);
+    }
+    validateExpr(expr);
+
+    const result = evaluateLinearExpr(expr);
+
+    return Number(result);
+}
+
+function evaluateLinearExpr(expr) {
+    expr = expr.replace(/\s/g, '');
+
+    divideMultipleExpr = /(\d*\.?\d+(?:[Ee][\+\-]?\d+)?)([/*])(\-?\d*\.?\d+(?:[Ee][\+\-]?\d+)?)/;
+    sumSubstrExpr = /(\-?\d*\.?\d+(?:[Ee][\+\-]?\d+)?)([+-])(\-?\d*\.?\d+(?:[Ee][\+\-]?\d+)?)/;
+
+    expr = expr.replace(/(\+-)|(-\+)/g, '-');
+    expr = expr.replace(/(\+\+)|(--)/g, '+');
+
+    while (divideMultipleExpr.test(expr)) {
+        expr = expr.replace(divideMultipleExpr, (match, a, operator, b) => simpleCalculator(a, operator, b));
+    }
+
+    while (sumSubstrExpr.test(expr)) {
+        expr = expr.replace(sumSubstrExpr, (match, a, operator, b) => simpleCalculator(a, operator, b));
+    }
+
+    return expr;
+}
+
+const simpleCalculator = (a, operator, b) => {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '/':
+            if (b == 0) {
+                throw new Error('TypeError: Division by zero.');
+            }
+            return a / b;
+        case '*':
+            return a * b;
+    }
+};
+
+const validateExpr = (expr) => {
+    regExp = /[()]/;
+    if (regExp.test(expr)) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    };
 }
 
 module.exports = {
